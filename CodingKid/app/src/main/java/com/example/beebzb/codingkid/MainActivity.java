88eb82f784a93a3a0dc2fb3ff3386ca93db364e5
Martin Trebula <Martin Trebula>
2016-11-09 21:58:46 +0100

    welcome text added

--- CodingKid/app/src/main/java/com/example/beebzb/codingkid/MainActivity.java
+++ CodingKid/app/src/main/java/com/example/beebzb/codingkid/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.beebzb.codingkid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.welcome)
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick (R.id.play_button)
     public void openLevelsActivity(){
-
+    title.setAllCaps(true);
     }
 
     @OnClick (R.id.editor_button)
     public void openEditorActivity(){
-
+    title.setAllCaps(false);
     }
 
     @OnClick (R.id.settings_button)
